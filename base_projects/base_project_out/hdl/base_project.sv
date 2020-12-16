b97b36e13486259f5977888b876d99adf12a19af
Jan Marjanovic <jan.marjanovic@outlook.com>
2020-12-16 21:25:54 +0100

    base_project: small changes to out project

--- base_projects/base_project_out/hdl/base_project.sv
+++ base_projects/base_project_out/hdl/base_project.sv
@@ -1,7 +1,7 @@
 
 
-module base_project (
-  output test_pin
+module base_project_osc  (
+    output osc_out
 );
 
 (*keep*) wire osc0, osc1, osc2;
@@ -10,6 +10,17 @@ not u0(osc1, osc0);
 not u1(osc2, osc1);
 not u2(osc0, osc2);
 
-assign test_pin = osc0;
+assign osc_out = osc0;
+
+endmodule
+
+
+module base_project (
+  output test_pin
+);
+
+base_project_osc inst_osc (
+    .osc_out (test_pin)
+);
 
 endmodule

92244ab86b5d8f3592e839f24812f60a937cedb6
Jan Marjanovic <jan.marjanovic@outlook.com>
2021-01-03 20:19:24 +0100

    extract_pin_addr: improve classifier, validation

--- scripts/extract_pin_addr/IOclassifier.py
+++ scripts/extract_pin_addr/IOclassifier.py
@@ -1,4 +1,5 @@
 import pickle
+import sys
 from collections import namedtuple
 
 import numpy as np
@@ -7,8 +8,12 @@ from JicBitstream import JicBitstream
 from knowledge import PU_ADDR
 from knowledge2 import BLK_LOC_START_BIT, UNKNW_BLK_LOWER_LIM, UNKWN_BLK_NR
 
+sys.path.append("../gen_bitstreams")
+from PinInfoParser import PinInfoParser
+
 IOSTD_REL_TO_PU = np.array(
     [
+        -1056,
         -512,
         -480,
         -448,
@@ -34,15 +39,20 @@ IOSTD_REL_TO_PU = np.array(
         1504,
         1536,
         1600,
+        1664,
+        1696,
     ]
 )
 
-IOclassifierOut = namedtuple("IOclassifierOut", ["inp", "out", "pu", "io_std", "term"])
+IOclassifierOut = namedtuple(
+    "IOclassifierOut", ["inp", "out", "pu", "io_std", "term", "diff"]
+)
 
 
 class IOclassifier:
     IDX_PU = 0
     IDX_INPUT_ACT_B = 288
+    IDX_INPUT_ACT_B_DIFF = -1056
 
     # output will have at least one of those set, input will have all zeros
     IDX_OUTPUT = [-512, -480, -416, -384, -320, -288, -256, -224, -192, -160, -128, 544]
@@ -52,6 +62,12 @@ class IOclassifier:
 
     IDX_OUT_DT = pickle.load(open("IDX_OUT_DT.p", "rb"))
     dtree_out = pickle.load(open("out_std_dtc.p", "rb"))
+
+    STRATIXV_PIN_INFO = "../../resources/5sgsd5.txt"
+    parser = PinInfoParser(STRATIXV_PIN_INFO, "F1517")
+    pins_info = parser.get_all_pins()
+    IDX_IO_STD_RX_DIFF = 1664
+    IDX_IO_STD_TX_DIFF = 1696
 
     idx_vec = IOSTD_REL_TO_PU
 
@@ -101,7 +117,8 @@ class IOclassifier:
         feat = jic.get_els(feat_addrs).astype(int)
         has_pu = self._has_pull_up(feat)
         has_output = self._has_output(feat)
-        has_input = self._has_input(feat)
+        diff = self._has_diff(feat, pin)
+        has_input = self._has_input(feat, diff)
         io_std = self._get_iostd(feat)
 
         if io_std[0][0] == "S":
@@ -110,7 +127,12 @@ class IOclassifier:
             sstl_term = None
 
         return IOclassifierOut(
-            inp=has_input, out=has_output, pu=has_pu, io_std=io_std[0], term=sstl_term
+            inp=has_input,
+            out=has_output,
+            pu=has_pu,
+            io_std=io_std[0],
+            term=sstl_term,
+            diff=diff,
         )
 
     def _has_pull_up(self, feat):
@@ -121,8 +143,11 @@ class IOclassifier:
         out_idxs = self.get_indices(self.idx_vec, self.IDX_OUTPUT)
         return np.any(feat[out_idxs])
 
-    def _has_input(self, feat):
-        in_idx = np.where(self.idx_vec == self.IDX_INPUT_ACT_B)[0][0]
+    def _has_input(self, feat, diff):
+        if diff:
+            in_idx = np.where(self.idx_vec == self.IDX_INPUT_ACT_B_DIFF)[0][0]
+        else:
+            in_idx = np.where(self.idx_vec == self.IDX_INPUT_ACT_B)[0][0]
         feat_input = feat[in_idx]
         return not bool(feat_input)
 
@@ -144,3 +169,12 @@ class IOclassifier:
             return "SSTL cl1/2, term"
         else:
             return "no term"
+
+    def _has_diff(self, feat, pin):
+        if self.pins_info[pin].tx_rx_ch.find("DIFFIO_RX") == 0:
+            diff_idx = np.where(self.idx_vec == self.IDX_IO_STD_RX_DIFF)[0][0]
+        else:
+            diff_idx = np.where(self.idx_vec == self.IDX_IO_STD_TX_DIFF)[0][0]
+
+        feat_diff = feat[diff_idx]
+        return bool(feat_diff)

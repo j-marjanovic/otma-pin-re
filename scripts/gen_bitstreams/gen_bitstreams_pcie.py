a066ecdf208186dffa9380fa7eb4e894980b739e
Jan Marjanovic <jan.marjanovic@outlook.com>
2021-05-06 21:06:56 +0200

    dev-pcie: find PCIe vendor ID position

--- scripts/gen_bitstreams/gen_bitstreams_pcie.py
+++ scripts/gen_bitstreams/gen_bitstreams_pcie.py
@@ -2,6 +2,8 @@
 
 import copy
 import dataclasses
+import os
+import xml.etree.ElementTree
 from queue import Empty, Queue
 from threading import Lock
 from typing import List
@@ -19,8 +21,8 @@ class PcieConfig:
     class_code: int = 0
     subsystem_device_id: int = 0
     subsystem_vendor_id: int = 0
-    bar0_type: int = 0
-    bar0_size_mask: int = 0
+    bar0_type: int = 2
+    bar0_size_mask: int = 10
     bar2_type: int = 0
     bar2_size_mask: int = 0
 
@@ -51,13 +53,41 @@ class EcoRunnerThreadPcie(EcoRunnerThread):
     BASE_PRJ_DIR = "../../base_projects/base_project_pcie"
     RESULTS_SUBDIR = "pcie"
 
+    def _get_qsys_pcie_filename(self):
+        return os.path.join(self.prj_dir, "qsys", "system_pcie.qsys")
+
+    def _modify_qsys_pcie(self, pcie_config):
+        """Update qsys elements"""
+
+        self.f_log.write("> _modify_qsys_pcie\n")
+        qsys_path = self._get_qsys_pcie_filename()
+        self.f_log.write(f">     {qsys_path}\n")
+
+        et = xml.etree.ElementTree.parse(qsys_path)
+
+        r = et.getroot()
+        mods = r.findall("module")
+        assert len(mods) == 1
+        m = mods[0]
+
+        for k, v in dataclasses.asdict(pcie_config).items():
+            els = m.findall(f".//*[@name='{k}_hwtcl']")
+            assert len(els) == 1
+
+            el = els[0]
+            el.set("value", str(v))
+
+        et.write(qsys_path)
+
     def run(self):
         try:
             while True:
                 conf = self.work_queue.get(block=False)
 
                 self.log_to_file(f">>> conf {conf} <<<")
-                # self.gen_qsys()
+
+                self._modify_qsys_pcie(conf)
+                self.gen_qsys()
                 self.compile_fpga_project()
                 self.store_results(f"pcie_{conf.get_compact_str()}.zip")
 
